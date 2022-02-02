733e83c399cc7ce63e69c20f14c486682836af98
John B <john.bang12@gmail.com>
2022-02-01 20:11:39 -0800

    webbsocket

--- src/components/edit-student.component.js
+++ src/components/edit-student.component.js
@@ -56,7 +56,7 @@ export default class EditStudent extends Component {
       rollno: this.state.rollno
     };
 
-    axios.put('/students/update-student/' + this.props.match.params.id, studentObject)
+    axios.put('http://localhost:5000/students/update-student/' + this.props.match.params.id, studentObject)
       .then((res) => {
         console.log(res.data)
         console.log('Student successfully updated')
